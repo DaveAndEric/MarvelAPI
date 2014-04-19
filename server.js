d0694349159373c66165c00bf74a036f97c5a336
David Thomas <davethomas691@gmail.com>
2014-04-19 14:36:41 -0400

    Adding Jade, Stylus, Express and tempaltes from http://clock.co.uk/blog/a-simple-website-in-nodejs-with-express-jade-and-stylus

--- server.js
+++ server.js
@@ -1,6 +1,44 @@
-var http = require('http')
+/*
+ * Module dependencies
+ */
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+
+var app = express()
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  )
+})
+
+
+/*var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
-}).listen(port);
+}).listen(port);*/
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  )
+})
+
+app.listen(1337)
